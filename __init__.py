7a9d550ff42c4f3ec8309216bf6aabd0e326c541
Benjamin Otto <git@otto.bayern>
2026-01-19 17:27:56 +0100

    Initial commit

---
+++ __init__.py
@@ -0,0 +1,16 @@
+"""Top-level package for mistral_llm."""
+
+__all__ = [
+    "NODE_CLASS_MAPPINGS",
+    "NODE_DISPLAY_NAME_MAPPINGS",
+    
+]
+
+__author__ = """otjamin"""
+__email__ = "git@otto.bayern"
+__version__ = "0.0.1"
+
+from .src.mistral_llm.nodes import NODE_CLASS_MAPPINGS
+from .src.mistral_llm.nodes import NODE_DISPLAY_NAME_MAPPINGS
+
+
